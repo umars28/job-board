1cf6ce59a2496482bbaa0d22006bd8d6d8f04899
Umar <umarsabirin369@gmail.com>
2025-06-17 12:01:17 +0800

    Standardize mapping indexing when indexing old data with indexing new data job

--- src/main/java/com/job/board/service/ElasticsearchDocIndexService.java
+++ src/main/java/com/job/board/service/ElasticsearchDocIndexService.java
@@ -1,6 +1,8 @@
 package com.job.board.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.job.board.entity.Job;
+import com.job.board.model.JobDocument;
 import org.elasticsearch.client.Request;
 import org.elasticsearch.client.RestClient;
 import org.springframework.stereotype.Service;
@@ -11,13 +13,24 @@ import java.io.IOException;
 public class ElasticsearchDocIndexService {
 
     private final RestClient restClient;
+    private final ObjectMapper objectMapper;
 
-    public ElasticsearchDocIndexService(RestClient restClient) {
+    public ElasticsearchDocIndexService(RestClient restClient, ObjectMapper objectMapper) {
         this.restClient = restClient;
+        this.objectMapper = objectMapper;
     }
 
     public void indexJob(Job job) throws IOException {
-        String jobJson = "{ \"id\": \"" + job.getId() + "\", \"title\": \"" + job.getTitle() + "\", \"description\": \"" + job.getDescription() + "\" }";
+        JobDocument doc = new JobDocument();
+        doc.setId(job.getId());
+        doc.setTitle(job.getTitle());
+        doc.setDescription(job.getDescription());
+        doc.setLocation(job.getLocation());
+        doc.setStatus(job.getStatus().name());
+        doc.setCompanyName(job.getCompany().getName());
+        doc.setCategoryName(job.getCategory() != null ? job.getCategory().getName() : null);
+
+        String jobJson = objectMapper.writeValueAsString(doc);
 
         Request request = new Request("PUT", "/jobs/_doc/" + job.getId());
         request.setJsonEntity(jobJson);
